888fe98425971b2f2d1a2dc20325d498bdd8ed81
Nick Cummings <nickmcummings@gmail.com>
2021-12-22 14:46:53 -0800

    Add the base games to the list.

--- src/data/games.js
+++ src/data/games.js
@@ -1,1 +1,17 @@
-export default ["Final Fantasy", "Final Fantasy II", "Final Fantasy III"];
+export default [
+  "Final Fantasy",
+  "Final Fantasy II",
+  "Final Fantasy III",
+  "Final Fantasy IV",
+  "Final Fantasy V",
+  "Final Fantasy VI",
+  "Final Fantasy VII",
+  "Final Fantasy VIII",
+  "Final Fantasy IX",
+  "Final Fantasy X",
+  "Final Fantasy XI",
+  "Final Fantasy XII",
+  "Final Fantasy XIII",
+  "Final Fantasy XIV",
+  "Final Fantasy XV",
+];
