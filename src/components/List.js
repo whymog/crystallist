2a1bb3969b65b91c95a72231896af11d82f2d5cd
Nick Cummings <nickmcummings@gmail.com>
2021-12-27 18:02:39 -0800

    Add some notes about what List should handle presentationally.

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,9 @@
 // TODO: Move the list logic and local state into this function component
 
-function List() {}
+function List() {
+  // TODO: Handle cursor state here. Use a setState() simply to listen for mouseDown/mouseUp to toggle between
+  // cursor: grab and cursor: grabbing in CSS.
+  // also TODO: get all that CSS out of index.css lol that's not where it goes. App.css, maybe, but not index!!
+}
 
 export default List;
