900fca446b55ae6ecec71d35ae7d3e778443802f
Nick Cummings <nickmcummings@gmail.com>
2021-12-29 11:03:00 -0800

    Implement hooks-based react-beautiful-dnd.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,105 @@
-import { /* useEffect, */ useState } from "react";
-import styles from "./App.module.css";
-
-import List from "./components/List";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styled from "@emotion/styled";
 
 import { mainGames } from "./data/games";
 
-// TODO:
-// - Store list in state
-// - Allow list updating by rearranging <li>s
-// - Represent list state in query string
-// - Parse list state from query string on load
-// - CSS polish
-// - Mobile interaction polish
+const initial = Array.from(mainGames).map((game, i) => {
+  return {
+    id: `id-${i}`,
+    content: game,
+  };
+});
 
-const defaultState = {
-  listItems: [...mainGames],
+const grid = 8;
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
 };
 
-const setInitialStateFromQueryString = (queryParams) => {
-  // TODO: Set filter mode, conditions, etc. from query string.
-  // Layer in localStorage data afterward if present.
-  // For now, though, let's just mimic the default behavior.
+const Item = styled.div`
+  width: 200px;
+  border: 1px solid grey;
+  margin-bottom: ${grid}px;
+  background-color: lightblue;
+  padding: ${grid}px;
+`;
 
-  return { ...defaultState };
-};
+function ItemWrapper({ item, index }) {
+  return (
+    <Draggable draggableId={item.id} index={index}>
+      {(provided) => (
+        <Item
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {item.content}
+        </Item>
+      )}
+    </Draggable>
+  );
+}
 
-const updateList = (newState, setState) => {};
+const ItemList = React.memo(function ItemList({ items }) {
+  return items.map((item, index) => (
+    <ItemWrapper item={item} index={index} key={item.id} />
+  ));
+});
 
 function App() {
   // Set initial state
-  const queryString = window.location.search;
-  let queryParams = new URLSearchParams(queryString);
+  // const queryString = window.location.search;
+  // let queryParams = new URLSearchParams(queryString);
 
-  let foo = queryParams.get("foo");
-  if (foo) {
-    // Do something
-  }
+  // let foo = queryParams.get("foo");
+  // if (foo) {
+  //   // Do something
+  // }
 
   // TODO: Check for query params, then localStorage, before pulling from the default list
-  const [state, setState] = useState(
-    queryParams?.length
-      ? setInitialStateFromQueryString(queryParams)
-      : { ...defaultState }
-  );
-
-  if (setState) {
-    // Just putting this in here temporarily to get rid of the linter warning
-  }
+  // const [state, setState] = useState(
+  //   queryParams?.length
+  //     ? setInitialStateFromQueryString(queryParams)
+  //     : { ...defaultState }
+  // );
 
   // TODO: Validate initialState from query string and weed out errors
+  const [state, setState] = useState({ items: initial });
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const items = reorder(
+      state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState({ items });
+  }
 
   return (
-    <div className={styles.main}>
-      <div className={styles.listWrapper}>
-        <List items={state.listItems} updateList={updateList} />
-      </div>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <ItemList items={state.items} />
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
