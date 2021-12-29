68d33599df0141c41622894768ef6ea9f86512dd
Nick Cummings <nickmcummings@gmail.com>
2021-12-29 11:15:10 -0800

    Begin figuring out more robust data structure for games.

--- src/data/games.js
+++ src/data/games.js
@@ -1,3 +1,28 @@
+const series = {
+  ff1: "FF1",
+  ff2: "FF2",
+  ff3: "FF3",
+  ff4: "FF4",
+  ff5: "FF5",
+  ff6: "FF6",
+  ff7: "FF7",
+  ff8: "FF8",
+  ff9: "FF9",
+  ff10: "FF10",
+  ff11: "FF11",
+  ff12: "FF12",
+  ff13: "FF13",
+  ff14: "FF14",
+  ff15: "FF15",
+  tactics: "TACTICS",
+  theatrhythm: "THEATRHYTHM",
+};
+
+const types = {
+  main: "MAIN",
+  mainRelated: "MAIN-RELATED",
+};
+
 export const mainGames = [
   "Final Fantasy",
   "Final Fantasy II",
@@ -14,4 +39,13 @@ export const mainGames = [
   "Final Fantasy XIII",
   "Final Fantasy XIV",
   "Final Fantasy XV",
+];
+
+// Figuring out a data structure that makes sense here
+export const allGames = [
+  {
+    name: "Final Fantasy",
+    type: types.main,
+    series: series.ff1,
+  },
 ];
